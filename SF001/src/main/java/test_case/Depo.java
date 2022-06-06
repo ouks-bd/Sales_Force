c0088d4a88b68a1c48d332e6fff737d7a3719e4f
ouks-bd <obyed.oss@gmail.com>
2022-06-06 11:51:44 +0600

    log_in_depo

--- SF001/src/main/java/test_case/Depo.java
+++ SF001/src/main/java/test_case/Depo.java
@@ -1,6 +1,30 @@
 package test_case;
 
-public class Depo {
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import base.Driver;
+import pom.PageObjectModel;
+
+public class Depo extends Driver {
+	
+String baseUrl="http://salesforce.oss.com.bd/";
+	
+	@Test
+	public void locatorlearning() throws InterruptedException {
+		
+		driver.get(baseUrl);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+
+		
+		PageObjectModel user=new PageObjectModel();
+		driver.findElement(By.id("UserName")).sendKeys("yeamin");
+		driver.findElement(By.id("Password")).sendKeys("@@");
+		driver.findElement(By.className("col-xs-4")).click();
  
 	
+}
 }
