8b106dbe7d5a5a0b7329d432e354ae74400feb5d
ouks-bd <obyed.oss@gmail.com>
2022-06-09 16:11:41 +0600

    r

--- SF001/src/main/java/test_case/Product.java
+++ SF001/src/main/java/test_case/Product.java
@@ -3,6 +3,8 @@ package test_case;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 import base.Driver;
@@ -26,6 +28,29 @@ String baseUrl="http://salesforce.oss.com.bd/";
 		driver.findElement(By.className("col-xs-4")).click();
 		
 		driver.get("http://salesforce.oss.com.bd/common/product");
+		
+		WebElement CatID = driver.findElement(By.id("CatID"));
+		Select Cat =new Select(CatID);
+		Cat.selectByValue("8");
+		
+		WebElement SubCatID = driver.findElement(By.id("SubCatID"));
+		Select SubCat =new Select(SubCatID);
+		SubCat.selectByValue("13");
+		
+		WebElement Common_Manufacturer = driver.findElement(By.id("Common_ManufacturerFK"));
+		Select Common_ManufacturerFK =new Select(Common_Manufacturer);
+		Common_ManufacturerFK.selectByValue("7");
+		
+		driver.findElement(By.id("PackPcs")).sendKeys("24");
+		
+		WebElement UnitFK = driver.findElement(By.id("UnitFK"));
+		Select Unit =new Select(UnitFK);
+		Unit.selectByValue("4");
+		
+		
+		
+		
+		
 
 }
 }
