82dc8237e7fce6317699828d7d77aeb73598dbfa
ouks-bd <obyed.oss@gmail.com>
2022-06-11 15:47:00 +0600

    product login

--- SF001/src/main/java/test_case/ProductSettings.java
+++ SF001/src/main/java/test_case/ProductSettings.java
@@ -1,7 +1,29 @@
 package test_case;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
 import base.Driver;
+import pom.PageObjectModel;
 
 public class ProductSettings extends Driver {
+		
+	
+	
+String baseUrl="http://salesforce.oss.com.bd/";
+	
+	@Test
+	public void Salesforce() throws InterruptedException {
+		
+		driver.get(baseUrl);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
 
+		
+		PageObjectModel user=new PageObjectModel();
+		driver.findElement(By.id("UserName")).sendKeys(user.UserNameCompany);
+		driver.findElement(By.id("Password")).sendKeys(user.Companypassword);
+		driver.findElement(By.className("col-xs-4")).click();
 }
