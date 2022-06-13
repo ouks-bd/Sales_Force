68f8fbb63264ccc1dcb8be0797d21d6f1e53f38c
ouks-bd <obyed.oss@gmail.com>
2022-06-13 10:22:53 +0600

    manufacturer

---
+++ SF001/src/main/java/configuration/ManageManufacturer.java
@@ -0,0 +1,35 @@
+package configuration;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import base.Driver;
+import pom.PageObjectModel;
+
+public class ManageManufacturer extends Driver {
+	
+String baseUrl="http://salesforce.oss.com.bd/";
+	
+	@Test
+	public void Salesforce() throws InterruptedException {
+		
+		driver.get(baseUrl);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+
+		
+		PageObjectModel user=new PageObjectModel();
+		driver.findElement(By.id("UserName")).sendKeys(user.UserNameCompany);
+		driver.findElement(By.id("Password")).sendKeys(user.Companypassword);
+		driver.findElement(By.className("col-xs-4")).click();
+		
+		driver.get("http://salesforce.oss.com.bd/common/manufacturer");
+	
+		
+	
+	}
+	
+
+}
