98f33825ca44fa607ebc34612c4bcfcc5d3eff6f
ouks-bd <obyed.oss@gmail.com>
2022-06-14 12:53:23 +0600

    charge type

--- SF001/src/main/java/ScmSettings/ManageChargeType.java
+++ SF001/src/main/java/ScmSettings/ManageChargeType.java
@@ -1,7 +1,39 @@
 package ScmSettings;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import base.Driver;
+import pom.PageObjectModel;
+
 public class ManageChargeType extends Driver {
 
 	
+String baseUrl="http://salesforce.oss.com.bd/";
 	
+	@Test
+	public void Salesforce() throws InterruptedException {
+		
+		driver.get(baseUrl);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+
+		
+		PageObjectModel user=new PageObjectModel();
+		driver.findElement(By.id("UserName")).sendKeys(user.UserNameCompany);
+		driver.findElement(By.id("Password")).sendKeys(user.Companypassword);
+		driver.findElement(By.className("col-xs-4")).click();
+		
+		driver.get("http://salesforce.oss.com.bd/common/chargetype");
+		
+		driver.findElement(By.id("Name")).sendKeys("Demo Charge");
+		driver.findElement(By.id("IsPercent")).click();
+		driver.findElement(By.id("Amount")).sendKeys("20");
+		
+		//driver.findElement(By.id("btnsave")).click();
+		
+		Thread.sleep(5000);
+}
 }
