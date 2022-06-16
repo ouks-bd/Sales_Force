fba42b307ba44c16597a1295c4cc4df5b0bdbc5c
ouks-bd <obyed.oss@gmail.com>
2022-06-16 17:33:04 +0600

    link

--- SF001/src/main/java/distributionsettings/ManageEmployeeForDistributer.java
+++ SF001/src/main/java/distributionsettings/ManageEmployeeForDistributer.java
@@ -12,7 +12,7 @@ import pom.PageObjectModel;
 
 public class ManageEmployeeForDistributer extends Driver {
 	
-String baseUrl="http://salesforce.oss.com.bd/saleslead/assignroutetouser";
+String baseUrl="http://salesforce.oss.com.bd/saleslead/assignusertodealer";
 	
 	@Test
 	public void Salesforce() throws InterruptedException {
