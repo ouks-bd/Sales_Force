202fffeb292fe090bdafc7e235bd74e1e956cc26
ouks-bd <obyed.oss@gmail.com>
2022-06-18 10:49:13 +0600

    print

--- SF001/src/main/java/productsettings/ProductSettings.java
+++ SF001/src/main/java/productsettings/ProductSettings.java
@@ -31,6 +31,9 @@ String baseUrl="http://salesforce.oss.com.bd/";
 		
 		driver.findElement(By.id("Name")).sendKeys("Biscuits");
 		
+		System.out.println("Raw Item");
+		
+		
 		driver.get("http://salesforce.oss.com.bd/common/rawsubcategory");
 		
 		WebElement DealerID = driver.findElement(By.id("RawCategoryFK"));
@@ -38,6 +41,26 @@ String baseUrl="http://salesforce.oss.com.bd/";
 		Dealer.selectByValue("1");
 		
 		driver.findElement(By.id("Name")).sendKeys("POP Corn");
+		
+		driver.get("http://salesforce.oss.com.bd/common/rawitem");
+		
+		WebElement ForRaw = driver.findElement(By.id("RawCategoryFK"));
+		Select ForRa=new Select(ForRaw);
+		ForRa.selectByValue("1");
+		
+		
+		WebElement RawSubCategoryFK= driver.findElement(By.id("RawSubCategoryFK"));
+		Select RawSubCategory=new Select(RawSubCategoryFK);
+		RawSubCategory.selectByValue("1");
+		
+		driver.findElement(By.id("Name")).sendKeys("POP Corn");
+		
+		
+		WebElement UnitFK= driver.findElement(By.id("UnitFK"));
+		Select Unit=new Select(UnitFK);
+		Unit.selectByValue("1");
+		
+		
 		
 		
 		Thread.sleep(5000);
