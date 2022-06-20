8703144601f9d644b157c5d42907edcab9d43ce2
ouks-bd <obyed.oss@gmail.com>
2022-06-20 13:05:21 +0600

    ss

--- SF001/src/main/java/purchaseorder/PurchaseOrder.java
+++ SF001/src/main/java/purchaseorder/PurchaseOrder.java
@@ -1,5 +1,31 @@
 package purchaseorder;
 
-public class PurchaseOrder  {
+import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import base.Driver;
+import pom.PageObjectModel;
+
+public class PurchaseOrder extends Driver  {
+	
+String baseUrl="http://salesforce.oss.com.bd/";
+	
+	@Test
+	public void Salesforce() throws InterruptedException {
+		
+		driver.get(baseUrl);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+
+		
+		PageObjectModel user=new PageObjectModel();
+		driver.findElement(By.id("UserName")).sendKeys(user.UserNameCompany);
+		driver.findElement(By.id("Password")).sendKeys(user.Companypassword);
+		driver.findElement(By.className("col-xs-4")).click();
+		
+		driver.get("http://salesforce.oss.com.bd/order/receive");
+
+}
 }
