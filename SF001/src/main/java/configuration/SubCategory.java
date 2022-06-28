50dfa0f562e6b0988c01a9f35a1eed40bdd2d552
ouks-bd <obyed.oss@gmail.com>
2022-06-28 15:58:42 +0600

    n

--- SF001/src/main/java/configuration/SubCategory.java
+++ SF001/src/main/java/configuration/SubCategory.java
@@ -12,7 +12,7 @@ import pom.PageObjectModel;
 
 public class SubCategory extends Driver{
 	
-String baseUrl="http://salesforce.oss.com.bd/";
+	String baseUrl="https://sf.bdbizhub.com/";
 	
 	@Test
 	public void Salesforce() throws InterruptedException {
